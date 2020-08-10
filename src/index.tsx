3df6a810fa4d69bed803eecf8eaab268dd7459d0
ken-nah <kennyadduz@gmail.com>
2020-08-10 15:25:01 +0300

    Final polishing

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga";
 
 import "./index.scss";
 
 import App from "./App";
+
+ReactGA.initialize(process.env.REACT_APP_GA!);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
